1d0e4460b40b6835fa484744de45328902a5e0cf
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-18 12:40:06 +0200

    feat: wire visible-content authority into schema runtime

--- packages/seo-geo-core/src/Runtime.php
+++ packages/seo-geo-core/src/Runtime.php
@@ -22,6 +22,7 @@ use SeoGeo\Core\Schema\SchemaIdentityResolver;
 use SeoGeo\Core\Schema\SchemaLocalBusinessResolver;
 use SeoGeo\Core\Schema\SchemaNodeIds;
 use SeoGeo\Core\Schema\SchemaPresenter;
+use SeoGeo\Core\Schema\SchemaVisibleContentResolver;
 use SeoGeo\Core\Seo\BreadcrumbResolver;
 use SeoGeo\Core\Seo\CanonicalResolver;
 use SeoGeo\Core\Seo\HreflangResolver;
@@ -142,9 +143,10 @@ final class Runtime {
 
 		$schema_ids            = new SchemaNodeIds();
 		$schema_breadcrumb     = new SchemaBreadcrumbResolver( self::$breadcrumbs, $schema_ids );
-		$schema_identity       = new SchemaIdentityResolver( $schema_ids );
-		$schema_local_business = new SchemaLocalBusinessResolver( $schema_ids, $schema_identity );
-		$schema_article        = new SchemaArticleResolver( $schema_identity );
+		$schema_identity        = new SchemaIdentityResolver( $schema_ids );
+		$schema_visible_content = new SchemaVisibleContentResolver();
+		$schema_local_business  = new SchemaLocalBusinessResolver( $schema_ids, $schema_identity, $schema_visible_content );
+		$schema_article         = new SchemaArticleResolver( $schema_identity );
 		self::$schema_graph    = new SchemaGraphBuilder(
 			$indexability,
 			$canonical,
