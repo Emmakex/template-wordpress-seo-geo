aaf5edd0146a2d7ba7426019e599d9a7f5e46700
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-18 12:04:31 +0200

    feat: wire LocalBusiness resolver into runtime

--- packages/seo-geo-core/src/Runtime.php
+++ packages/seo-geo-core/src/Runtime.php
@@ -19,6 +19,7 @@ use SeoGeo\Core\Schema\SchemaArticleResolver;
 use SeoGeo\Core\Schema\SchemaBreadcrumbResolver;
 use SeoGeo\Core\Schema\SchemaGraphBuilder;
 use SeoGeo\Core\Schema\SchemaIdentityResolver;
+use SeoGeo\Core\Schema\SchemaLocalBusinessResolver;
 use SeoGeo\Core\Schema\SchemaNodeIds;
 use SeoGeo\Core\Schema\SchemaPresenter;
 use SeoGeo\Core\Seo\BreadcrumbResolver;
@@ -141,8 +142,9 @@ final class Runtime {
 
 		$schema_ids         = new SchemaNodeIds();
 		$schema_breadcrumb  = new SchemaBreadcrumbResolver( self::$breadcrumbs, $schema_ids );
-		$schema_identity    = new SchemaIdentityResolver( $schema_ids );
-		$schema_article     = new SchemaArticleResolver( $schema_identity );
+		$schema_identity       = new SchemaIdentityResolver( $schema_ids );
+		$schema_local_business = new SchemaLocalBusinessResolver( $schema_ids, $schema_identity );
+		$schema_article        = new SchemaArticleResolver( $schema_identity );
 		self::$schema_graph = new SchemaGraphBuilder(
 			$indexability,
 			$canonical,
@@ -150,6 +152,7 @@ final class Runtime {
 			$schema_ids,
 			$schema_breadcrumb,
 			$schema_identity,
+			$schema_local_business,
 			$schema_article
 		);
 		$schema_presenter   = new SchemaPresenter( self::$seo_authority, self::$schema_graph );
