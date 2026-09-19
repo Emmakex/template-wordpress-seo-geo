78e643c83a4c4a5594f03b6ab0483aaf9d3befb1
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-19 18:37:30 +0200

    feat: allow Ecommerce preset activation

--- packages/seo-geo-theme/inc/presets.php
+++ packages/seo-geo-theme/inc/presets.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return list<string>
  */
 function seo_geo_theme_preset_ids(): array {
-	return array( 'corporate', 'local-business', 'publisher' );
+	return array( 'corporate', 'local-business', 'publisher', 'ecommerce' );
 }
 
 /**
