7b7a569f899f30cc17339ae4c1c55b839255eaba
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-25 19:03:20 +0200

    style: align finalization service coalescing assignments

--- packages/seo-geo-migration-bridge/src/Plugin.php
+++ packages/seo-geo-migration-bridge/src/Plugin.php
@@ -485,9 +485,9 @@ final class Plugin {
 		self::$clone_import_file_controller      ??= new AdminCloneImportFileController( self::$clone_import_file_restorer );
 		self::$clone_import_rewrite_state_store  ??= new ImportRewriteStateStore();
 		self::$clone_import_environment_rewriter ??= new ImportEnvironmentRewriter( self::$clone_import_rewrite_state_store, self::$clone_import_state_store, self::$clone_import_database_state_store, self::$clone_import_file_state_store, self::$clone_import_database_restorer, self::$clone_job_store );
-		self::$clone_import_rewrite_controller    ??= new AdminCloneImportRewriteController( self::$clone_import_environment_rewriter );
-		self::$clone_import_finalize_state_store  ??= new ImportFinalizeStateStore();
-		self::$clone_import_finalization_planner  ??= new ImportFinalizationPlanner(
+		self::$clone_import_rewrite_controller   ??= new AdminCloneImportRewriteController( self::$clone_import_environment_rewriter );
+		self::$clone_import_finalize_state_store ??= new ImportFinalizeStateStore();
+		self::$clone_import_finalization_planner ??= new ImportFinalizationPlanner(
 			self::$clone_import_finalize_state_store,
 			self::$clone_import_state_store,
 			self::$clone_import_payload_state_store,
