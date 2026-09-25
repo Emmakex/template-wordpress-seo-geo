e7447921304afef8618676f04a21bc2f45c0c486
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-25 19:03:23 +0200

    style: align finalization controller assignments

--- packages/seo-geo-migration-bridge/src/Clone/AdminCloneImportFinalizeController.php
+++ packages/seo-geo-migration-bridge/src/Clone/AdminCloneImportFinalizeController.php
@@ -53,20 +53,20 @@ final class AdminCloneImportFinalizeController {
 			);
 		}
 
-		$job_id = $this->posted_job_id();
+		$job_id          = $this->posted_job_id();
 		check_admin_referer( self::NONCE_ACTION . ':' . $job_id );
 
-		$batch_rows = isset( $_POST['finalize_batch_rows'] )
+		$batch_rows      = isset( $_POST['finalize_batch_rows'] )
 			? absint( sanitize_text_field( wp_unslash( $_POST['finalize_batch_rows'] ) ) )
 			: ImportFinalizationPlanner::DEFAULT_BATCH_ROWS;
-		$batch_files = isset( $_POST['finalize_batch_files'] )
+		$batch_files     = isset( $_POST['finalize_batch_files'] )
 			? absint( sanitize_text_field( wp_unslash( $_POST['finalize_batch_files'] ) ) )
 			: ImportFinalizationPlanner::DEFAULT_BATCH_FILES;
 		$batch_megabytes = isset( $_POST['finalize_batch_megabytes'] )
 			? absint( sanitize_text_field( wp_unslash( $_POST['finalize_batch_megabytes'] ) ) )
 			: 16;
 
-		$result = $this->planner->advance(
+		$result   = $this->planner->advance(
 			$job_id,
 			$batch_rows,
 			$batch_files,
