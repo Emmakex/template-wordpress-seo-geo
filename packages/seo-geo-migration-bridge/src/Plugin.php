4db4af1e098d01de3d0d9149d427eafe165d6b01
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-25 16:56:48 +0200

    feat: wire staging-file restore services

--- packages/seo-geo-migration-bridge/src/Plugin.php
+++ packages/seo-geo-migration-bridge/src/Plugin.php
@@ -14,6 +14,7 @@ use SeoGeo\MigrationBridge\Clone\AdminCloneInventoryController;
 use SeoGeo\MigrationBridge\Clone\AdminCloneImportController;
 use SeoGeo\MigrationBridge\Clone\AdminCloneImportPayloadController;
 use SeoGeo\MigrationBridge\Clone\AdminCloneImportDatabaseController;
+use SeoGeo\MigrationBridge\Clone\AdminCloneImportFileController;
 use SeoGeo\MigrationBridge\Clone\AdminCloneDatabaseExportController;
 use SeoGeo\MigrationBridge\Clone\AdminCloneFileExportController;
 use SeoGeo\MigrationBridge\Clone\AdminClonePackageController;
@@ -30,6 +31,8 @@ use SeoGeo\MigrationBridge\Clone\ImportPayloadStateStore;
 use SeoGeo\MigrationBridge\Clone\ImportPayloadVerifier;
 use SeoGeo\MigrationBridge\Clone\ImportDatabaseRestorer;
 use SeoGeo\MigrationBridge\Clone\ImportDatabaseStateStore;
+use SeoGeo\MigrationBridge\Clone\ImportFileRestorer;
+use SeoGeo\MigrationBridge\Clone\ImportFileStateStore;
 use SeoGeo\MigrationBridge\Clone\ImportPreflight;
 use SeoGeo\MigrationBridge\Clone\ImportStateStore;
 use SeoGeo\MigrationBridge\Clone\PackageBuilder;
@@ -350,6 +353,27 @@ final class Plugin {
 	private static ?AdminCloneImportDatabaseController $clone_import_database_controller = null;
 
 	/**
+	 * Portable Clone staging-file restore state store singleton.
+	 *
+	 * @var ImportFileStateStore|null
+	 */
+	private static ?ImportFileStateStore $clone_import_file_state_store = null;
+
+	/**
+	 * Portable Clone staging-file restorer singleton.
+	 *
+	 * @var ImportFileRestorer|null
+	 */
+	private static ?ImportFileRestorer $clone_import_file_restorer = null;
+
+	/**
+	 * Portable Clone staging-file restore controller singleton.
+	 *
+	 * @var AdminCloneImportFileController|null
+	 */
+	private static ?AdminCloneImportFileController $clone_import_file_controller = null;
+
+	/**
 	 * Read-only final migration report engine singleton.
 	 *
 	 * @var MigrationReportEngine|null
@@ -408,6 +432,9 @@ final class Plugin {
 		self::$clone_import_database_state_store ??= new ImportDatabaseStateStore();
 		self::$clone_import_database_restorer    ??= new ImportDatabaseRestorer( self::$clone_import_database_state_store, self::$clone_import_state_store, self::$clone_import_payload_state_store, self::$clone_job_store, self::$clone_import_preflight );
 		self::$clone_import_database_controller  ??= new AdminCloneImportDatabaseController( self::$clone_import_database_restorer );
+		self::$clone_import_file_state_store     ??= new ImportFileStateStore();
+		self::$clone_import_file_restorer        ??= new ImportFileRestorer( self::$clone_import_file_state_store, self::$clone_import_state_store, self::$clone_import_payload_state_store, self::$clone_import_database_state_store, self::$clone_job_store, self::$clone_import_preflight );
+		self::$clone_import_file_controller      ??= new AdminCloneImportFileController( self::$clone_import_file_restorer );
 
 		self::$incremental_baseline_capture ??= new IncrementalBaselineCapture();
 
@@ -431,6 +458,7 @@ final class Plugin {
 		self::$clone_import_controller->boot();
 		self::$clone_import_payload_controller->boot();
 		self::$clone_import_database_controller->boot();
+		self::$clone_import_file_controller->boot();
 		self::$operator_screen->register();
 	}
 
@@ -565,6 +593,13 @@ final class Plugin {
 	 */
 	public static function clone_import_database_restorer(): ?ImportDatabaseRestorer {
 		return self::$clone_import_database_restorer;
+	}
+
+	/**
+	 * Return the resumable Portable Clone staging-file restorer.
+	 */
+	public static function clone_import_file_restorer(): ?ImportFileRestorer {
+		return self::$clone_import_file_restorer;
 	}
 
 	/**
