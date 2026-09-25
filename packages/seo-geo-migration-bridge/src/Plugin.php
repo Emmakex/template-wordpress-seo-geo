583a604a8e271734d3f4bb4dcde590110b3c724b
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-25 20:23:15 +0200

    Align 0.8.22 promotion service bootstrap

--- packages/seo-geo-migration-bridge/src/Plugin.php
+++ packages/seo-geo-migration-bridge/src/Plugin.php
@@ -495,19 +495,19 @@ final class Plugin {
 	 * Initialize Migration Bridge services.
 	 */
 	public static function boot(): void {
-		self::$analyzer               ??= new SiteAnalyzer();
-		self::$baseline_snapshotter   ??= new BaselineSnapshotter();
-		self::$dependency_graph       ??= new DependencyGraphBuilder();
-		self::$sandbox_lab            ??= new SandboxMigrationLab();
-		self::$migration_engine       ??= new MigrationEngine();
-		self::$migration_controller   ??= new AdminMigrationController( self::$migration_engine );
-		self::$parity_engine          ??= new SeoParityEngine();
-		self::$cutover_engine         ??= new CutoverEngine();
-		self::$cutover_controller     ??= new AdminCutoverController( self::$cutover_engine );
-		self::$operator_status        ??= new OperatorStatus();
-		self::$operator_screen        ??= new AdminOperatorScreen( self::$operator_status );
-		self::$migration_report       ??= new MigrationReportEngine();
-		self::$migration_report_store ??= new MigrationReportStore();
+		self::$analyzer                                     ??= new SiteAnalyzer();
+		self::$baseline_snapshotter                         ??= new BaselineSnapshotter();
+		self::$dependency_graph                             ??= new DependencyGraphBuilder();
+		self::$sandbox_lab                                  ??= new SandboxMigrationLab();
+		self::$migration_engine                             ??= new MigrationEngine();
+		self::$migration_controller                         ??= new AdminMigrationController( self::$migration_engine );
+		self::$parity_engine                                ??= new SeoParityEngine();
+		self::$cutover_engine                               ??= new CutoverEngine();
+		self::$cutover_controller                           ??= new AdminCutoverController( self::$cutover_engine );
+		self::$operator_status                              ??= new OperatorStatus();
+		self::$operator_screen                              ??= new AdminOperatorScreen( self::$operator_status );
+		self::$migration_report                             ??= new MigrationReportEngine();
+		self::$migration_report_store                       ??= new MigrationReportStore();
 
 		self::$clone_job_store                              ??= new CloneJobStore();
 		self::$clone_controller                             ??= new AdminCloneController( self::$clone_job_store );
@@ -553,37 +553,36 @@ final class Plugin {
 			self::$clone_import_database_restorer,
 			self::$clone_job_store
 		);
-		self::$clone_import_finalize_controller              ??= new AdminCloneImportFinalizeController( self::$clone_import_finalization_planner );
-		self::$clone_import_database_activation_state_store  ??= new ImportDatabaseActivationStateStore();
-		self::$clone_import_file_promotion_state_store       ??= new ImportFilePromotionStateStore();
-		self::$clone_import_database_activator               ??= new ImportDatabaseActivator(
+		self::$clone_import_finalize_controller             ??= new AdminCloneImportFinalizeController( self::$clone_import_finalization_planner );
+		self::$clone_import_database_activation_state_store ??= new ImportDatabaseActivationStateStore();
+		self::$clone_import_file_promotion_state_store      ??= new ImportFilePromotionStateStore();
+		self::$clone_import_database_activator              ??= new ImportDatabaseActivator(
 			self::$clone_import_database_activation_state_store,
 			self::$clone_import_finalization_planner,
 			self::$clone_import_file_promotion_state_store
 		);
-		self::$clone_import_database_activation_controller   ??= new AdminCloneImportDatabaseActivationController( self::$clone_import_database_activator );
+		self::$clone_import_database_activation_controller  ??= new AdminCloneImportDatabaseActivationController( self::$clone_import_database_activator );
 
-
-		self::$clone_import_file_promotion_planner            ??= new ImportFilePromotionPlanner(
+		self::$clone_import_file_promotion_planner          ??= new ImportFilePromotionPlanner(
 			self::$clone_import_file_promotion_state_store,
 			self::$clone_import_database_activation_state_store,
 			self::$clone_import_file_state_store,
 			self::$clone_import_finalize_state_store
 		);
-		self::$clone_import_file_promoter                     ??= new ImportFilePromoter(
+		self::$clone_import_file_promoter                   ??= new ImportFilePromoter(
 			self::$clone_import_file_promotion_state_store,
 			self::$clone_import_file_promotion_planner,
 			self::$clone_import_database_activation_state_store,
 			self::$clone_job_store
 		);
-		self::$clone_import_file_promotion_controller         ??= new AdminCloneImportFilePromotionController( self::$clone_import_file_promoter );
+		self::$clone_import_file_promotion_controller       ??= new AdminCloneImportFilePromotionController( self::$clone_import_file_promoter );
 
-		self::$incremental_baseline_capture ??= new IncrementalBaselineCapture();
+		self::$incremental_baseline_capture                 ??= new IncrementalBaselineCapture();
 
-		self::$baseline_capture_controller ??= new AdminBaselineCaptureController( self::$incremental_baseline_capture );
+		self::$baseline_capture_controller                  ??= new AdminBaselineCaptureController( self::$incremental_baseline_capture );
 
-		self::$sandbox_handoff_controller   ??= new AdminSandboxHandoffController();
-		self::$dependency_review_controller ??= new AdminDependencyReviewController();
+		self::$sandbox_handoff_controller                   ??= new AdminSandboxHandoffController();
+		self::$dependency_review_controller                 ??= new AdminDependencyReviewController();
 
 		SandboxGuard::boot();
 		self::$migration_controller->boot();
