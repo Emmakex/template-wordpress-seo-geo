50eac3c6ab5986ab369ee912e44c06e2cd129885
Edu <133777880+Emmakex@users.noreply.github.com>
2026-09-26 08:08:24 +0200

    Align target preflight plugin wiring

--- packages/seo-geo-migration-bridge/src/Plugin.php
+++ packages/seo-geo-migration-bridge/src/Plugin.php
@@ -721,8 +721,8 @@ final class Plugin {
 		self::$clone_import_state_store                     ??= new ImportStateStore();
 		self::$clone_import_preflight                       ??= new ImportPreflight( self::$clone_import_state_store, self::$clone_job_store );
 		self::$clone_import_controller                      ??= new AdminCloneImportController( self::$clone_import_preflight );
-		self::$local_clone_target_preflight_state_store      ??= new LocalCloneTargetPreflightStateStore();
-		self::$local_clone_target_preflight                  ??= new LocalCloneTargetPreflight(
+		self::$local_clone_target_preflight_state_store     ??= new LocalCloneTargetPreflightStateStore();
+		self::$local_clone_target_preflight                 ??= new LocalCloneTargetPreflight(
 			self::$local_clone_target_preflight_state_store,
 			self::$local_clone_package_handoff,
 			self::$local_clone_sandbox_runtime_bootstrapper,
@@ -731,7 +731,7 @@ final class Plugin {
 			self::$clone_import_state_store,
 			self::$clone_job_store
 		);
-		self::$local_clone_target_preflight_controller       ??= new AdminCloneLocalTargetPreflightController( self::$local_clone_target_preflight );
+		self::$local_clone_target_preflight_controller      ??= new AdminCloneLocalTargetPreflightController( self::$local_clone_target_preflight );
 		self::$clone_import_payload_state_store             ??= new ImportPayloadStateStore();
 		self::$clone_import_payload_verifier                ??= new ImportPayloadVerifier( self::$clone_import_payload_state_store, self::$clone_import_state_store, self::$clone_job_store, self::$clone_import_preflight );
 		self::$clone_import_payload_controller              ??= new AdminCloneImportPayloadController( self::$clone_import_payload_verifier );
